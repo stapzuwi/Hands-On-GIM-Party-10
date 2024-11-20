af601c50597bc36738d1982a4b8380892c7a1c1f
Nusss0 <stevanus610@gmail.com>
2024-11-21 00:52:02 +0700

    ChangeScene
    Menambahkan Mainscene and Deathscene

--- Assets/PlayerHitDetector.cs
+++ Assets/PlayerHitDetector.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHitDetector : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public Collider2D playerCollider;
-    
+    public string Scene;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other == playerCollider)
         {
-            playerMovement.isDead = true;    
+            playerMovement.isDead = true; 
+            SceneManager.LoadScene(Scene);
         }
     }
 
